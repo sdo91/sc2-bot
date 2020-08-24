4e788cf16f906e66becbea232ba739d518b1bafd
Scott Oehler <sdo91@gmx.com>
2020-08-23 20:03:27 -0600

    added build_assimilator

--- bots/resonator_bot.py
+++ bots/resonator_bot.py
@@ -16,21 +16,22 @@ from sc2.unit import Unit
 from sc2.units import Units
 from sc2.position import Point2
 from sc2.player import Bot, Computer
-from wave import Wave
+from bots.wave import Wave
 
 class ResonatorBot(sc2.BotAI):
+
     resonating_glaves = False
     waves: ['Wave'] = []
-
-    def on_upgrade_complete(self, upgrade: UpgradeId):
-        if upgrade == BuffId.RESONATINGGLAIVESPHASESHIFT:
-            print("UPGRADE COMPLETE")
-            self.resonating_glaves = True
 
     def __init__(self):
         super().__init__()
         self.save_minerals = False
         self.save_vespene = False
+
+    def on_upgrade_complete(self, upgrade: UpgradeId):
+        if upgrade == BuffId.RESONATINGGLAIVESPHASESHIFT:
+            print("UPGRADE COMPLETE")
+            self.resonating_glaves = True
 
     def can_afford(self, item_id: Union[UnitTypeId, UpgradeId, AbilityId], check_supply_cost: bool = True) -> bool:
         cost = self.calculate_cost(item_id)
@@ -76,6 +77,8 @@ class ResonatorBot(sc2.BotAI):
 
         await self.build_gateways(nexus, 1)
 
+        self.build_assimilator(nexus)
+
         await self.build_structure(UnitTypeId.CYBERNETICSCORE, nexus)
 
         await self.build_gateways(nexus, 2)
@@ -87,33 +90,6 @@ class ResonatorBot(sc2.BotAI):
         self.do_upgrades()
 
         self.make_army()
-
-        # # If we have no forge, build one near the pylon that is closest to our starting nexus
-        # elif not self.structures(UnitTypeId.FORGE):
-        #     pylon_ready = self.structures(UnitTypeId.PYLON).ready
-        #     if pylon_ready:
-        #         if self.can_afford(UnitTypeId.FORGE):
-        #             await self.build(UnitTypeId.FORGE, near=pylon_ready.closest_to(nexus))
-        #
-        # # If we have less than 2 pylons, build one at the enemy base
-        # elif self.structures(UnitTypeId.PYLON).amount < 2:
-        #     if self.can_afford(UnitTypeId.PYLON):
-        #         pos = self.enemy_start_locations[0].towards(self.game_info.map_center, random.randrange(8, 15))
-        #         await self.build(UnitTypeId.PYLON, near=pos)
-        #
-        # # If we have no cannons but at least 2 completed pylons, automatically find a placement location and build them near enemy start location
-        # elif not self.structures(UnitTypeId.PHOTONCANNON):
-        #     if self.structures(UnitTypeId.PYLON).ready.amount >= 2 and self.can_afford(UnitTypeId.PHOTONCANNON):
-        #         pylon = self.structures(UnitTypeId.PYLON).closer_than(20, self.enemy_start_locations[0]).random
-        #         await self.build(UnitTypeId.PHOTONCANNON, near=pylon)
-        #
-        # # Decide if we should make pylon or cannons, then build them at random location near enemy spawn
-        # elif self.can_afford(UnitTypeId.PYLON) and self.can_afford(UnitTypeId.PHOTONCANNON):
-        #     # Ensure "fair" decision
-        #     for _ in range(20):
-        #         pos = self.enemy_start_locations[0].random_on_distance(random.randrange(5, 12))
-        #         building = UnitTypeId.PHOTONCANNON if self.state.psionic_matrix.covers(pos) else UnitTypeId.PYLON
-        #         await self.build(building, near=pos)
 
     def make_probes(self, nexus):
         # Make probes until we have 16 total
@@ -141,6 +117,30 @@ class ResonatorBot(sc2.BotAI):
 
         # build a pylon
         return await self.build(UnitTypeId.PYLON, near=nexus)
+
+    def build_assimilator(self, nexus):
+        """
+        Build gas near completed nexus
+        """
+        vgs = self.vespene_geyser.closer_than(15, nexus)
+        for vg in vgs:
+            if self.gas_buildings.closer_than(1, vg):
+                # there is already a building
+                # todo: assign workers
+                return
+
+            if not self.can_afford(UnitTypeId.ASSIMILATOR):
+                self.save_for(UnitTypeId.ASSIMILATOR)
+                return
+
+            worker = self.select_build_worker(vg.position)
+            if worker is None:
+                # worker not available
+                return
+
+            # else build
+            worker.build(UnitTypeId.ASSIMILATOR, vg)
+            worker.stop(queue=True)
 
     async def build_structure(self, id, nexus, cap=1, save=False):
         if self.structures(id).amount < cap:
