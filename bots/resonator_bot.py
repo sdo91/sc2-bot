13008dd2d406e340455833267ad5b96ab28befeb
Scott Oehler <sdo91@gmx.com>
2020-09-13 22:19:49 -0600

    fixed duplicate structure bug

--- bots/resonator_bot.py
+++ bots/resonator_bot.py
@@ -1,32 +1,37 @@
-import sys, os
+import os
+import sys
 from typing import Union
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-import random
-
 import sc2
 from sc2 import Race, Difficulty
 from sc2.ids.unit_typeid import UnitTypeId
-from sc2.ids.buff_id import BuffId
 from sc2.ids.ability_id import AbilityId
 from sc2.ids.upgrade_id import UpgradeId
 from sc2.ids.buff_id import BuffId
-from sc2.unit import Unit
-from sc2.units import Units
-from sc2.position import Point2
+from sc2.unit import UnitOrder
 from sc2.player import Bot, Computer
-from bots.wave import Wave
+
 
 class ResonatorBot(sc2.BotAI):
-
-    resonating_glaves_started = False
-    waves: ['Wave'] = []
+    """
+    todo:
+        add pylon build logic
+        expand to 2nd base
+        research warp gate
+        use tech_requirement_progress
+        do something else with chronoboost after glaves are done
+        build different unit types
+    """
 
     def __init__(self):
         super().__init__()
+
         self.save_minerals = False
         self.save_vespene = False
+
+        self.resonating_glaves_started = False
 
     async def on_upgrade_complete(self, upgrade: UpgradeId):
         if upgrade == BuffId.RESONATINGGLAIVESPHASESHIFT:
@@ -68,8 +73,6 @@ class ResonatorBot(sc2.BotAI):
         else:
             nexus = self.townhalls.random
 
-        await self.distribute_workers()
-
         # order these by priority
 
         self.make_probes(nexus)
@@ -80,11 +83,13 @@ class ResonatorBot(sc2.BotAI):
 
         self.build_assimilators(nexus)
 
-        await self.build_structure(UnitTypeId.CYBERNETICSCORE, nexus)
+        if self.structures(UnitTypeId.GATEWAY).ready:
+            await self.build_structure(UnitTypeId.CYBERNETICSCORE, nexus)
 
         await self.build_gateways(nexus, 2)
 
-        await self.build_structure(UnitTypeId.TWILIGHTCOUNCIL, nexus)
+        if self.structures(UnitTypeId.CYBERNETICSCORE).ready:
+            await self.build_structure(UnitTypeId.TWILIGHTCOUNCIL, nexus)
 
         await self.build_gateways(nexus, 4)
 
@@ -95,6 +100,10 @@ class ResonatorBot(sc2.BotAI):
         self.make_army()
 
         self.do_attack()
+
+        await self.distribute_workers()
+
+        self.check_duplicate_structures()
 
     def make_probes(self, nexus):
         # Make probes until we have enough
@@ -131,7 +140,6 @@ class ResonatorBot(sc2.BotAI):
         for vg in vgs:
             if self.gas_buildings.closer_than(1, vg):
                 # there is already a building
-                # todo: assign workers
                 return
 
             if not self.can_afford(UnitTypeId.ASSIMILATOR):
@@ -147,19 +155,53 @@ class ResonatorBot(sc2.BotAI):
             worker.build(UnitTypeId.ASSIMILATOR, vg)
             worker.stop(queue=True)
 
-    async def build_structure(self, id, nexus, cap=1, save=False):
-        if self.structures(id).amount < cap:
+    async def build_structure(self, unit_id, nexus, cap=1, save=False):
+        # todo: add logic to build pylon if needed
+        if self.structures(unit_id).amount < cap:
             pylon = self.structures(UnitTypeId.PYLON).ready
             if pylon:
-                if self.can_afford(id):
-                    await self.build(id, near=pylon.closest_to(nexus))
-                elif save:
-                    self.save_for(id)
+                if self.can_afford(unit_id):
+                    # we should build the structure
 
-            # todo: add logic to build pylon if needed
+                    if self.is_build_ordered():
+                        # a probe is already processing a build order
+                        return False
+
+                    result = await self.build(unit_id, near=pylon.closest_to(nexus))
+                    print("building: {}, {}".format(unit_id, result))
+                    return result
+                elif save:
+                    self.save_for(unit_id)
+        return False
+
+    def is_build_ordered(self):
+        """
+        sometimes 2 building get queued at the same time
+        this is to prevent that from happening
+
+        return True if there is currently a worker with a build order
+        """
+        workers_not_collecting = [w for w in self.workers if not w.is_collecting]
+        for w in workers_not_collecting:
+            for order in w.orders:  # type: UnitOrder
+                order_name = order.ability.friendly_name
+                if 'Build' in order_name:
+                    print('already ordered: {}'.format(order_name))
+                    return True
+        return False
 
     async def build_gateways(self, nexus, cap, save=False):
         await self.build_structure(UnitTypeId.GATEWAY, nexus, cap=cap, save=save)
+
+    def check_duplicate_structures(self):
+        to_check = [
+            UnitTypeId.TWILIGHTCOUNCIL,
+            UnitTypeId.CYBERNETICSCORE,
+        ]
+        for unit_id in to_check:
+            amount = self.structures(unit_id).amount
+            if amount > 1:
+                print("duplicate structures detected: {}".format([unit_id, amount]))
 
     def do_chronoboost(self, nexus):
         if nexus.energy < 50:
@@ -217,10 +259,15 @@ class ResonatorBot(sc2.BotAI):
 
 
 def main():
+    # difficulty = Difficulty.Easy
+    # difficulty = Difficulty.Medium
+    difficulty = Difficulty.Hard
+
     sc2.run_game(
         sc2.maps.get("YearZeroLE"),
-        [Bot(Race.Protoss, ResonatorBot(), name="ResonatorBot"), Computer(Race.Protoss, Difficulty.Medium)],
-        realtime=True,
+        [Bot(Race.Protoss, ResonatorBot(), name="ResonatorBot"), Computer(Race.Protoss, difficulty)],
+        # realtime=True,
+        realtime=False,
     )
 
 
